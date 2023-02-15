b03df32d60c75632515c81f98bcf0830ae867ff1
Muyun <wangzehuan2017@sina.com>
2023-02-15 22:38:15 +0000

    v0.1

---
+++ api/proxy.js
@@ -0,0 +1,20 @@
+// the purpose of this file is to setup the proxy of vercel server
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+  let target = ''
+
+  // target server
+  if (req.url.startsWith('/api')) {
+    target = 'https://zehuan-wang.edinburgh.domains/fatfree/Volcano-Back/'
+  }
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      // rewrite the request url
+      '^/api/': '/'
+    }
+  })(req, res)
+}
+
--- src/views/VolcanoView.vue
+++ src/views/VolcanoView.vue
@@ -41,7 +41,7 @@ export default {
       volcano_name: 'ASH AND CINDER CONE NAME OF VOLCANO',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       volcano_json: [],
-      type: this.$route.query.type,
+      type: this.$route.query.type ? this.$route.query.type : 'Stratovolcano',
     };
   },
   methods: {
@@ -49,13 +49,17 @@ export default {
       this.$router.go(-1);
     },
     async getVolcano(type) {
-      await getVolcano(type).then((res) => {
-        if (res.data) {
-          this.volcano_json = res.data;
-        } else {
-          this.volcano_json = [];
-        }
-      });
+      await getVolcano(type)
+        .then((res) => {
+          if (res.data) {
+            this.volcano_json = res.data;
+          } else {
+            this.volcano_json = [];
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
   created() {
